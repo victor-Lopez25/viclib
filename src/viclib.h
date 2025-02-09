7dc4c3ae8eb8c5cdf9f624a3afc628c4a712a1e2
victhor <victorlopezcortes25@gmail.com>
2025-02-09 23:11:26 +0100

    Add assertion in arena api

--- src/viclib.h
+++ src/viclib.h
@@ -18,26 +18,26 @@ functions it uses
 
 LICENCES:
 tsoding string view implementation:
-// Copyright 2021 Alexey Kutepov <reximkut@gmail.com>
+Copyright 2021 Alexey Kutepov <reximkut@gmail.com>
 
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
 
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 License to viclib.h:
 Copyright (c) 2024 Víctor López
@@ -773,6 +773,7 @@ ARENAPROC size_t ArenaGetRemaining_align(memory_arena *Arena, size_t Alignment)
 
 ARENAPROC void *ArenaPushSize_align(memory_arena *Arena, size_t RequestSize, size_t Alignment)
 {
+    AssertMsg(Alignment != 0, "Alignment parameter must be at least 1");
     size_t Size = RequestSize;
     size_t AlignOffset = ArenaGetAlignmentOffset(Arena, Alignment);
     Size += AlignOffset;
