7d0c017ebe03ba710f30e406fb34aef421f798b0
victhor <victorlopezcortes25@gmail.com>
2026-03-03 12:49:07 +0100

    Test is working with more stuff

--- src/temp.c
+++ src/temp.c
@@ -25,114 +25,109 @@ int main(int argc, char **argv)
         "    \"hyper!\",\n"
         "    \"In the view\"\n"
         "  ]\n"
+        ""
+        "  \"another-object\": {\n"
+        "    \"empty-array\": [],\n"
+        "    \"empty-object\": {}\n"
+        "  },\n"
+        "  \"deep-object\": {\n"
+        "    \"deeper-object\": {\n"
+        "      \"deeperer-object\": {\n"
+        "        \"deep-arr\": [\n"
+        "          \"abcdef\",\n"
+        "          \"ghijkl\"\n"
+        "        ],\n"
+        "        \"value\": 10\n"
+        "      }\n"
+        "    }\n"
+        "  }\n"
         "}";
 
     vl_serialize_context ctx = GetDeserializeContext(SerializeType_JSON, .buffer = data, .buffer_size = sizeof(data));
 
     Assert(VL_ObjectBegin(&ctx));
-        if(VL_AttributeName(&ctx, "somenullobj")) {
-            Assert(VL_SerializeNull(&ctx));
-        }
+    if(VL_AttributeName(&ctx, "somenullobj")) {
+        Assert(VL_SerializeNull(&ctx));
+    }
 
-        if(VL_AttributeName(&ctx, "boolean-values")) {
-            Assert(VL_ArrayBegin(&ctx));
-                bool boolean;
-                Assert(VL_SerializeOpBool(&ctx, &boolean) && boolean);
-                Assert(VL_SerializeOpBool(&ctx, &boolean) && !boolean);
-            Assert(VL_ArrayEnd(&ctx));
-        }
+    if(VL_AttributeName(&ctx, "boolean-values")) {
+        Assert(VL_ArrayBegin(&ctx));
+            bool boolean;
+            Assert(VL_SerializeOpBool(&ctx, &boolean) && boolean);
+            Assert(VL_SerializeOpBool(&ctx, &boolean) && !boolean);
+        Assert(VL_ArrayEnd(&ctx));
+    }
 
-        if(VL_AttributeName(&ctx, "integers")) {
-            Assert(VL_ArrayBegin(&ctx));
-                int64_t val;
-                for(int i = 0; i < 4; i++) {
-                    Assert(VL_SerializeOpInt(&ctx, &val) && (val == i));
-                }
-            Assert(VL_ArrayEnd(&ctx));
-        }
-
-        if(VL_AttributeName(&ctx, "string-values")) {
-            Assert(VL_ArrayBegin(&ctx));
-                char *s;
-                Assert(VL_SerializeOpString(&ctx, &s) && !strcmp(s, "hyper!"));
-                free(s); // remember to free this!
-
-                view v;
-                Assert(VL_SerializeOpView(&ctx, &v) && ViewEq(v, VIEW("In the view")));
-            Assert(VL_ArrayEnd(&ctx));
-        }
-    Assert(VL_ObjectEnd(&ctx));
-
-#if 0
-    //vl_serialize_context ctx = GetSerializeContext(SerializeType_JSON, .indent = 2, .float_fmt = "%+lg");
-    //vl_serialize_context ctx = GetSerializeContext(SerializeType_XML, .indent = 2);
-    //vl_serialize_context ctx = GetSerializeContext(SerializeType_TOML, .indent = 2);
-    vl_serialize_context ctx = GetSerializeContext(SerializeType_C99_Initializer, .indent = 2);
-
-    VL_ObjectBegin(&ctx);
-        VL_AttributeName(&ctx, "somenullobj");
-        VL_SerializeNull(&ctx);
-
-        VL_AttributeName(&ctx, "boolean-values");
-        VL_ArrayBegin(&ctx);
-            VL_SerializeBool(&ctx, true);
-            VL_SerializeBool(&ctx, false);
-        VL_ArrayEnd(&ctx);
-
-        VL_AttributeName(&ctx, "integers");
-        VL_ArrayBegin(&ctx);
+    if(VL_AttributeName(&ctx, "integers")) {
+        Assert(VL_ArrayBegin(&ctx));
+            int64_t val;
             for(int i = 0; i < 4; i++) {
-                VL_SerializeInt(&ctx, i);
+                Assert(VL_SerializeOpInt(&ctx, &val) && (val == i));
             }
-        VL_ArrayEnd(&ctx);
+        Assert(VL_ArrayEnd(&ctx));
+    }
 
-        VL_AttributeName(&ctx, "floating-point-values");
-        VL_ArrayBegin(&ctx);
-            VL_SerializeFloat(&ctx, 0.0);
-            VL_SerializeFloat(&ctx, -0.0);
-            VL_SerializeFloat(&ctx, 134.576);
-            VL_SerializeFloat(&ctx, NAN);
-            VL_SerializeFloat(&ctx, INFINITY);
-        VL_ArrayEnd(&ctx);
+    if(VL_AttributeName(&ctx, "string-values")) {
+        Assert(VL_ArrayBegin(&ctx));
+            char *s;
+            Assert(VL_SerializeOpString(&ctx, &s) && !strcmp(s, "hyper!"));
+            free(s); // remember to free this!
 
-        VL_AttributeName(&ctx, "string-values");
-        VL_ArrayBegin(&ctx);
-            VL_SerializeString(&ctx, "hyper!");
-            VL_SerializeView(&ctx, VIEW("In the view"));
-        VL_ArrayEnd(&ctx);
+            view v;
+            Assert(VL_SerializeOpView(&ctx, &v) && ViewEq(v, VIEW("In the view")));
+        Assert(VL_ArrayEnd(&ctx));
+    }
 
-        VL_AttributeName(&ctx, "another-object");
-        VL_ObjectBegin(&ctx);
-            VL_AttributeName(&ctx, "empty-array");
-            VL_ArrayBegin(&ctx);
-            VL_ArrayEnd(&ctx);
+    /*
+    VL_AttributeName(&ctx, "floating-point-values");
+    VL_ArrayBegin(&ctx);
+        VL_SerializeFloat(&ctx, 0.0);
+        VL_SerializeFloat(&ctx, -0.0);
+        VL_SerializeFloat(&ctx, 134.576);
+        VL_SerializeFloat(&ctx, NAN);
+        VL_SerializeFloat(&ctx, INFINITY);
+    VL_ArrayEnd(&ctx);
+    */
 
-            VL_AttributeName(&ctx, "empty-object");
-            VL_ObjectBegin(&ctx);
-            VL_ObjectEnd(&ctx);
-        VL_ObjectEnd(&ctx);
+    if(VL_AttributeName(&ctx, "another-object")) {
+        Assert(VL_ObjectBegin(&ctx));
+            if(VL_AttributeName(&ctx, "empty-array")) {
+                Assert(VL_ArrayBegin(&ctx));
+                Assert(VL_ArrayEnd(&ctx));
+            }
 
-        VL_AttributeName(&ctx, "deep-object");
-        VL_ObjectBegin(&ctx);
-            VL_AttributeName(&ctx, "deeper-object");
-            VL_ObjectBegin(&ctx);
-                VL_AttributeName(&ctx, "deeperer-object");
-                VL_ObjectBegin(&ctx);
-                    VL_AttributeName(&ctx, "deep-arr");
-                    VL_ArrayBegin(&ctx);
-                        VL_SerializeString(&ctx, "abcdef");
-                        VL_SerializeString(&ctx, "ghijkl");
-                    VL_ArrayEnd(&ctx);
+            if(VL_AttributeName(&ctx, "empty-object")) {
+                Assert(VL_ObjectBegin(&ctx));
+                Assert(VL_ObjectEnd(&ctx));
+            }
+        Assert(VL_ObjectEnd(&ctx));
+    }
 
-                    VL_AttributeName(&ctx, "value");
-                    VL_SerializeInt(&ctx, 10);
-                VL_ObjectEnd(&ctx);
-            VL_ObjectEnd(&ctx);
-        VL_ObjectEnd(&ctx);
+    if(VL_AttributeName(&ctx, "deep-object")) {
+        Assert(VL_ObjectBegin(&ctx));
+        if(VL_AttributeName(&ctx, "deeper-object")) {
+            Assert(VL_ObjectBegin(&ctx));
+            if(VL_AttributeName(&ctx, "deeperer-object")) {
+                Assert(VL_ObjectBegin(&ctx));
+                    if(VL_AttributeName(&ctx, "deep-arr")) {
+                        Assert(VL_ArrayBegin(&ctx));
+                        view v;
+                        Assert(VL_SerializeOpView(&ctx, &v));
+                        Assert(VL_SerializeOpView(&ctx, &v));
+                        Assert(VL_ArrayEnd(&ctx));
+                    }
 
-    VL_ObjectEnd(&ctx);
-
-#endif
+                    if(VL_AttributeName(&ctx, "value")) {
+                        int64_t val;
+                        Assert(VL_SerializeOpInt(&ctx, &val));
+                    }
+                Assert(VL_ObjectEnd(&ctx));
+            }
+            Assert(VL_ObjectEnd(&ctx));
+        }
+        Assert(VL_ObjectEnd(&ctx));
+    }
+    Assert(VL_ObjectEnd(&ctx));
 
     return 0;
 }
