76bcc6ed60f5078808e11fb9e4a5f7f683566f4f
victhor <victorlopezcortes25@gmail.com>
2026-08-23 21:36:43 +0200

    cleanup needsrebuild context and add a couple of cases for VL_FILE_INVALID

--- vl_build.h
+++ vl_build.h
@@ -1,5 +1,5 @@
 // [vl_build.h](https://github.com/victor-Lopez25/viclib) © 2025 by [Víctor López Cortés](https://github.com/victor-Lopez25) is licensed under [CC BY 4.0](https://creativecommons.org/licenses/by/4.0/)
-// version: 1.5.3
+// version: 1.5.2
 #ifndef VL_BUILD_H
 #define VL_BUILD_H
 
@@ -285,38 +285,6 @@ VLIBPROC bool VL_GetLastWriteTime(const char *file, u64 *writeTime);
 // Only checks the filetime of all the input files vs output file
 #define VL_NeedsRebuild(out, in, ...) VL_NeedsRebuild_Impl(out, ((const char*[]){in, __VA_ARGS__}), sizeof((const char*[]){in, __VA_ARGS__})/sizeof(const char*))
 VLIBPROC int VL_NeedsRebuild_Impl(const char *output_path, const char **input_paths, size_t input_paths_count);
-
-typedef struct vl_filetime_node vl_filetime_node;
-struct vl_filetime_node {
-    view file;
-    u64 time;
-    //bool exploredAllDependencies;
-    vl_filetime_node *next;
-};
-
-#ifndef VL_BUILD_FILETIME_TABLE_SIZE
-#define VL_BUILD_FILETIME_TABLE_SIZE 1024
-#endif // VL_BUILD_FILETIME_TABLE_SIZE
-
-typedef struct {
-    vl_filetime_node *items;
-    size_t count;
-    size_t capacity;
-} vl_filetime_nodelist;
-
-typedef struct {
-    vl_filetime_nodelist nodes;
-    uint32_t countTimes;
-} vl_filetime_table;
-
-typedef struct {
-    vl_filetime_table table;
-    vl_filetime_node *freelist;
-    vl_file_paths *includePaths;
-    memory_arena *Arena;
-} vl_needrebuild_context;
-
-extern vl_needrebuild_context VL_needsRebuildContext;
 
 #ifndef VL_BUILD_FILENAME_HASH
 #define VL_BUILD_FILENAME_HASH(v, hash) do {\
@@ -830,6 +798,11 @@ VLIBPROC bool VL_CopyDirectoryRecursively_Impl(const char *src, const char *dst,
             VL_ReturnDefer(false);
         } break;
 
+        case VL_FILE_INVALID: {
+            VL_Log(VL_ERROR, "Cannot copy file of invalid type %s", src);
+            VL_ReturnDefer(false);
+        } break;
+
         default: Assert(!"Unreachable");
     }
 
@@ -894,6 +867,11 @@ VLIBPROC bool VL_ReadDirectoryFilesRecursively(const char *parent, vl_file_paths
 
         case VL_FILE_OTHER: {
             VL_Log(VL_ERROR, "Unsupported type of file %s", parent);
+            VL_ReturnDefer(false);
+        } break;
+
+        case VL_FILE_INVALID: {
+            VL_Log(VL_ERROR, "Could not read file of invalid type %s", parent);
             VL_ReturnDefer(false);
         } break;
 
